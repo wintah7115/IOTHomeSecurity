cd9f4560a05686c1229472b3f4d8a6b459525880
wintah7115 <aidenjblanchard@protonmail.com>
2024-11-21 16:30:48 -0500

    Added joint functionality to scripts and clearer template generation

--- Scripts/mail-send.py
+++ Scripts/mail-send.py
@@ -1,10 +1,12 @@
-
+#!../tmp/.venv/bin/virtualenv python
+import exportscanreport
 import os
 import smtplib
 import sys
-#import imghdr
+import traceback
+from datetime import datetime
 from email.message import EmailMessage 
-
+from time import sleep
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 PASS = os.getenv("EMAIL_PASS")
 REPORT = os.getenv("REPORT_NAME")
@@ -14,8 +16,14 @@ msg['Subject'] = 'IOT HOME SECURITY NOTIFICATION'
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = sys.argv[1]
 msg.set_content('test')
-
-
+print("Getting report")
+try:
+	exportscanreport.main()
+except Exception as e:
+	print("Error occurred during execution at " + str(datetime.now().date()) + " {}".format(datetime.now().time()))
+	print(traceback.format_exc())
+	print(e)
+print("Sending Message")
 with open(f'../Scans/{REPORT}', 'r', encoding='utf-8') as f:
 	file_data = f.read()
 	file_name = REPORT
