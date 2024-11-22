f2262bc6065111cc3e356f2a89c6e56278886945
wintah7115 <aidenjblanchard@protonmail.com>
2024-11-21 20:34:46 -0500

    automation

---
+++ cronscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/IOTPI/Desktop/IOTHomeSecurity/tmp/.venv/bin/activate
+cd /home/IOTPI/Desktop/IOTHomeSecurity/Scripts
+pipenv run python mail-send.py aidenjblanchard@protonmail.com
