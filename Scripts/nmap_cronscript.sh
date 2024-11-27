d210101fbe4cd9a87ca182309dc774a659950482
wintah7115 <aidenjblanchard@protonmail.com>
2024-11-27 15:50:17 -0500

    reorg

---
+++ Scripts/nmap_cronscript.sh
@@ -0,0 +1,2 @@
+
+sudo nmap 10.0.0.0/24 -O -p0-65535 -v -oX ./Scans/nmap_report.html --webxml --stylesheet --noninteractive --open
--- nmap_cronscript.sh
+++
@@ -1,2 +0,0 @@
-
-sudo nmap 172.20.10.0/28 -O -p- -v -oX ./Scans/nmap_report.txt --webxml --stylesheet --noninteractive --open
