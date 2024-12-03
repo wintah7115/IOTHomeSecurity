1cf20afceb632b2f30d7ea386d5216e630c58d15
aiden blanchard <aidenblanchard@aidenm.local>
2024-12-03 10:48:02 -0500

    annotations

--- Scripts/mail-send.py
+++ Scripts/mail-send.py
@@ -7,27 +7,27 @@ import traceback
 from datetime import datetime
 from email.message import EmailMessage 
 from time import sleep
-EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS") #get env variables
 PASS = os.getenv("EMAIL_PASS")
 REPORT = os.getenv("REPORT_NAME")
-
+#create message format
 msg = EmailMessage()
 msg['Subject'] = 'IOT HOME SECURITY NOTIFICATION' 
 msg['From'] = EMAIL_ADDRESS
 msg['To'] = sys.argv[1]
 msg.set_content('test')
 print("Getting report")
-try:
+try: #Run scan export
 	exportscanreport.main()
 except Exception as e:
 	print("Error occurred during execution at " + str(datetime.now().date()) + " {}".format(datetime.now().time()))
 	print(traceback.format_exc())
 	print(e)
 print("Sending Message")
-with open(f'../Scans/{REPORT}', 'r', encoding='utf-8') as f:
+with open(f'../Scans/{REPORT}', 'r', encoding='utf-8') as f: #insert report into email via html
 	file_data = f.read()
 	file_name = REPORT
-msg.add_alternative(file_data, subtype='html')
+msg.add_alternative(file_data, subtype='html') #send to SMTP server
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 	smtp.login(EMAIL_ADDRESS, PASS)
 
--- Scripts/nmap_cronscript.sh
+++ Scripts/nmap_cronscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/IOTPI/Desktop/IOTHomeSecurity/tmp/.venv/bin/activate
+source /home/IOTPI/Desktop/IOTHomeSecurity/tmp/.venv/bin/activate #activates python environment
 cd /home/IOTPI/Desktop/IOTHomeSecurity/Scripts
-pipenv run python nmap_mail_send.py aidenjblanchard@protonmail.com
+pipenv run python nmap_mail_send.py aidenjblanchard@protonmail.com #runs script
